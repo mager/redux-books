76ed0c119142a04556cd7bee1a83da7e98739bbd
Andrew Mager <mager@postmates.com>
2016-02-15 08:28:58 -0800

    Wired up

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Eloquent Ruby' },
+    { title: 'Harry Potter' },
+    { title: 'Shantaram' }
+  ]
+}
